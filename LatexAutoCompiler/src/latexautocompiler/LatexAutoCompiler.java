ad5d8bfdc8a9369972bda044f5a273041fe79eb4
Sourabh Bhat <heySourabh@gmail.com>
2015-11-22 00:46:33 +0530

    - Calling PdfLatex compilation method from the new class PdfLatexInterface, instead of being defined here.

--- LatexAutoCompiler/src/latexautocompiler/LatexAutoCompiler.java
+++ LatexAutoCompiler/src/latexautocompiler/LatexAutoCompiler.java
@@ -4,7 +4,6 @@ import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
@@ -38,8 +37,8 @@ public class LatexAutoCompiler {
             System.out.println("Updating tex file");
             System.out.println("Latest modified file: " + FileUtils.getLatestModifiedFile(dir));
             try {
-                runPdfLatex(texFile); // Run twice to take care of indexing
-                if (runPdfLatex(texFile) != 0) { // Check for error in compilation
+                PdfLatexInterface.runPdfLatex(texFile); // Run twice to take care of indexing
+                if (PdfLatexInterface.runPdfLatex(texFile) != 0) { // Check for error in compilation
                     throw new IllegalStateException("Check log file. \n"
                             + combineStrings(FileUtils.readLinesWithPattern(
                                             new File(texFile.getAbsolutePath().replace(".tex", ".log")),
@@ -60,34 +59,6 @@ public class LatexAutoCompiler {
         } catch (InterruptedException ex) {
             // Ignore
         }
-    }
-
-    /**
-     * Runs pdflatex and returns the status
-     *
-     * @param texFile
-     * @return status returned by pdflatex command
-     * @throws Exception
-     */
-    static private int runPdfLatex(File texFile) throws Exception {
-        File dir = texFile.getParentFile();
-        Process proc = Runtime.getRuntime().exec(
-                new String[]{
-                    "pdflatex",
-                    "-halt-on-error",
-                    "-file-line-error",
-                    texFile.getAbsolutePath()
-                },
-                null,
-                dir
-        );
-        try {
-            proc.waitFor(1, TimeUnit.MINUTES);
-            proc.destroy();
-        } catch (InterruptedException ex) {
-            // Ignore exception
-        }
-        return proc.exitValue();
     }
 
     static private String combineStrings(ArrayList<String> strings) {
