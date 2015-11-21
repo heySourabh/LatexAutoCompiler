33118d477b4a5374ae879b2bb0fd791cfb76a558
Sourabh Bhat <heySourabh@gmail.com>
2015-11-22 00:44:32 +0530

    - Removed unwanted methods from FileUtils

--- LatexAutoCompiler/src/latexautocompiler/FileUtils.java
+++ LatexAutoCompiler/src/latexautocompiler/FileUtils.java
@@ -2,7 +2,6 @@ package latexautocompiler;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.Pattern;
@@ -46,51 +45,7 @@ public class FileUtils {
 
         return latestModifiedFile;
     }
-
-    public static String tail(File file, int lines) {
-        java.io.RandomAccessFile fileHandler = null;
-        try {
-            fileHandler
-                    = new java.io.RandomAccessFile(file, "r");
-            long fileLength = fileHandler.length() - 1;
-            StringBuilder sb = new StringBuilder();
-            int line = 0;
-
-            for (long filePointer = fileLength; filePointer != -1; filePointer--) {
-                fileHandler.seek(filePointer);
-                int readByte = fileHandler.readByte();
-
-                if (readByte == 0xA) {
-                    if (filePointer < fileLength) {
-                        line = line + 1;
-                    }
-                } else if (readByte == 0xD) {
-                    if (filePointer < fileLength - 1) {
-                        line = line + 1;
-                    }
-                }
-                if (line >= lines) {
-                    break;
-                }
-                sb.append((char) readByte);
-            }
-
-            String lastLine = sb.reverse().toString();
-            return lastLine;
-        } catch (java.io.FileNotFoundException e) {
-            return null;
-        } catch (java.io.IOException e) {
-            return null;
-        } finally {
-            if (fileHandler != null) {
-                try {
-                    fileHandler.close();
-                } catch (IOException e) {
-                }
-            }
-        }
-    }
-
+    
     public static ArrayList<String> readLinesWithPattern(File file, Pattern pattern) {
         ArrayList<String> strs = new ArrayList<>();
         try {
